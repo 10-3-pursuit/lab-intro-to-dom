1c5d90ebc716f38083fb2b2e17d798102c2916d8
armandopires <armandopires@pursuit.org>
2023-10-29 03:15:01 -0400

    Did best I could

--- main.js
+++ main.js
@@ -1,34 +1,98 @@
-const firstSection = document.querySelector("section");
-firstSection.classList.add("featured");
+// Add a class of "featured" to the first section element
+// Make a querySelector in order to to add Feature.
+const section = document.querySelector("section")
+section.classList.add("featured");
+//*
 
+// Use create Element to make Article.
 const article = document.createElement("article");
+//*
 
-newArticle.innerHTML = 
-<article>
-  <img
-    src="./images/paul-gilmore-unsplash.jpg"
-    alt="Image of a mountain in front of a lake."
-  />
-  <h3>Stop Planning</h3>
-  <p>
-    You -- yes you! You're an over-planner, I can tell. It's time to stop
-    planning so much and instead focusing on relaxing. Taking a break at all is
-    so stressful these days; why add to the stress by overworking yourself?
-  </p>
-  <aside>
-    <p>
-      <span><strong>Read Time:</strong> 4 Minutes</span
-      ><a href="#">Read more...</a>
-    </p>
-  </aside>
-</article>
+// Create a image element.
+// Set Attribute and copy and paste both links.
+// Append last after create and set.
+const image = document.createElement("img");
+image.setAttribute("src", "./images/paul-gilmore-unsplash.jpg")
+image.setAttribute("alt", "Image of a mountain in front of a lake.")
+article.append(image);
+//*
 
-var section = document.querySelector("section.posts");
-sectionPosts.appendChild(newArticle); 
+// Create h3
+// Copy "Stop Planning" and add to innerText
+// Append last after copy.
+const headerThree = document.createElement("h3");
+headerThree.innerText = "Stop Planning"
+article.append(headerThree);
+//*
 
-sectionPosts.before(article[1], article[0]);
+// Create a p element
+// Copy and paste Paragraph into innerText
+// Append last.
+const paragraph = document.createElement("p");
+paragraph.innerText = `You -- yes you! You're an over-planner, I can tell. It's time to stop
+planning so much and instead focusing on relaxing. Taking a break at all is
+so stressful these days; why add to the stress by overworking yourself?`
+article.append (paragraph);
+//*
 
-sectionPosts.before(article[0], article[2]);
+
+// Create aside element
+// Create asideparagraph element
+// Copy and paste span and paste into innerhtml
+// Append the asideparagraph first.
+const aside = document.createElement("aside");
+const asideParagraph = document.createElement("p");
+const span = document.createElement('span');
+span.innerHTML = `<span><strong>Read Time:</strong> 4 Minutes</span>`;
+asideParagraph.append(span);
+//*
+
+// Create a Element for the Read more
+const start = document.createElement('RM')
+start.setAttribute('href', '#')
+start.textContent = 'Read more...';
+//*
+
+
+// Append the start,asideParagraph and aside.
+article.append(aside);
+asideParagraph.append(start);
+aside.append(asideParagraph);
+//*
+
+// Add the article to the end of section.posts
+// Use querySelector to select section.posts.
+// Append everything to article.
+const postsSection = document.querySelector('section.posts');
+const firstArticle = document.querySelector('section.posts articles');
+postsSection.append(firstArticle);
+//*
+
+// Remove All Posts Link
+// 'a' means anchor element inside the 'nav' within the 'header' and removes it.
+// .Remove will remove it.
+const headerLink = document.querySelector('header nav a');
+headerLink.remove();
+
+// Remove the span element from the featured post
+const featuredSpan = document.querySelector('aside p');
+const spanElement = document.querySelector('span');
+featuredSpan.removeChild(spanElement);
+//*
+
+// Remove all titles from non-featured posts
+const nonFeaturedPosts = document.querySelectorAll('ul');
+const allPosts = document.querySelector('li');
+nonFeaturedPosts.removeChild(allPosts);
+//*
+
+
+
+
+
+
+
+
 
 
 
